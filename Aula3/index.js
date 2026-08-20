8afcc76f53b6fdc315172256046ffedff2281210
Eduardo Luiz Gottems <92766854+EduardoGottems@users.noreply.github.com>
2026-08-20 23:28:32 +0000

    created the basic HTML and basics types of data on index.js

--- Aula3/index.js
+++ Aula3/index.js
@@ -1,3 +1,4 @@
 let animal = "Cat";
+let animalNum = 4.9
 
-console.log( animal + " is the coolest animal on the world" );
+console.log( animal + " is the coolest animal on the world\nThe avarage rate of " + animal + " is " + animalNum + " stars." );
